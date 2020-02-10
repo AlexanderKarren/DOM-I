3f92aaa77c35181dd5c759f433896a3d10233c13
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-02-10 10:53:08 -0700

    added selectors to index.js

--- js/index.js
+++ js/index.js
@@ -40,3 +40,51 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Nav Links
+document.querySelectorAll("a").forEach(function(element, i) {
+  element.textContent = siteContent["nav"][`nav-item-${i+1}`];
+});
+
+// CTA Image
+document.getElementById("cta-img").setAttribute('src', siteContent["cta"]["img-src"]);
+
+// CTA Heading and Button
+const ctaHeading = document.querySelector(".cta-text").firstElementChild;
+ctaHeading.textContent = siteContent["cta"]["h1"];
+ctaHeading.nextElementSibling.textContent = siteContent["cta"]["button"];
+
+// Top Content
+const topContentContainer = document.querySelector(".top-content");
+
+topContentContainer.firstElementChild.firstElementChild.textContent = siteContent["main-content"]["features-h4"]; // left h4 tag
+topContentContainer.firstElementChild.firstElementChild.nextElementSibling.textContent = siteContent["main-content"]["features-content"] // left p
+
+topContentContainer.lastElementChild.firstElementChild.textContent = siteContent["main-content"]["about-h4"]; // right h4 tag
+topContentContainer.lastElementChild.firstElementChild.nextElementSibling.textContent = siteContent["main-content"]["about-content"];
+
+// Middle Image
+document.getElementById("middle-img").setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+// Bottom Content
+const bottomContentContainer = document.querySelector(".bottom-content");
+
+bottomContentContainer.firstElementChild.firstElementChild.textContent = siteContent["main-content"]["services-h4"];
+bottomContentContainer.firstElementChild.firstElementChild.nextElementSibling.textContent = siteContent["main-content"]["services-content"];
+
+bottomContentContainer.firstElementChild.nextElementSibling.firstElementChild.textContent = siteContent["main-content"]["product-h4"];
+bottomContentContainer.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.textContent = siteContent["main-content"]["product-content"];
+
+bottomContentContainer.lastElementChild.firstElementChild.textContent = siteContent["main-content"]["vision-h4"];
+bottomContentContainer.lastElementChild.firstElementChild.nextElementSibling.textContent = siteContent["main-content"]["vision-content"];
+
+// Contact Section
+const contactSection = document.querySelector(".contact");
+
+contactSection.firstElementChild.textContent = siteContent["contact"]["contact-h4"];
+contactSection.firstElementChild.nextElementSibling.textContent = siteContent["contact"]["address"];
+contactSection.firstElementChild.nextElementSibling.nextElementSibling.textContent = siteContent["contact"]["phone"];
+contactSection.lastElementChild.textContent = siteContent["contact"]["email"];
+
+// Footer
+document.querySelector("footer").firstElementChild.textContent = siteContent["footer"]["copyright"];
